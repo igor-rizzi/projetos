d4593f29f5ad4db94c611fa93799b280c8e8afe1
Igor Henrique Rizzi <igor.rizzi2@gmail.com>
2021-09-16 15:51:27 -0300

    Update Main.cs

--- C#/SysBodega/SysBodega/Main.cs
+++ C#/SysBodega/SysBodega/Main.cs
@@ -1,18 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SysBodega {
-    class Main {
-        static void main(string[] args) {
+    class Mae {
+        static void Main(string[] args) {
             String nome;
             int numero = 0;
-            String opt;
+            int opt;
             int opcao;
             String idadeFun;
             Double salarioFun;
             int codCliente = 0;
             int codigoDrink = 0;
 
-           
+            List<Funcionario> bodegueiros = new List<Funcionario>();
+            List<Bebida> bebidas = new List<Bebida>();
+            List<Cliente> clientes = new List<Cliente>();
+
+            Console.WriteLine("Seja muito bem-vindo ao sistema da Bodega do Juaum!");
+            Console.WriteLine("Informe o que você quer fazer: \n1 - Criar funcionário \n2 - Listar Funcionários \n3 - Adicionar bebida \n4 - Listar bebidas \n5 - Comprar bebida \n6 - Vender bebida \n7 - Adicionar cliente \n8 - Listar clientes \n9 - Sair");
+            opt = Console.ReadLine();
+
+            if(opt == 1) {
+
+            }
 
 
 
