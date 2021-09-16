538a0aeefbbcdb4e6fb9f1a0aac41b6d3a6d2c54
Igor Henrique Rizzi <igor.rizzi2@gmail.com>
2021-09-16 14:49:31 -0300

    Update Cliente.cs

--- C#/SysBodega/SysBodega/Cliente.cs
+++ C#/SysBodega/SysBodega/Cliente.cs
@@ -1,6 +1,26 @@
 ﻿using System;
 
-public class Class1
-{
-	
+public class Cliente{
+	private String nomeCliente;
+	private int codigoCliente;
+	private String cpf;
+	private Boolean vendaFiado;
+
+	public Cliente(String nomeCli, String cpfCliente, int codCliente, int venderFiado) {
+		nomeCliente = nomeCli;
+		cpf = cpfCliente;
+		codigoCliente = codCliente;
+    }
+
+	void vendeFiado(int venderFiado) {
+		if(venderFiado == 1) {
+			Console.WriteLine("Este cliente pode comprar fiado!");
+			vendaFiado = true;
+        }else if (venderFiado == 2){
+			Console.WriteLine("Este cliente não pode comprar fiado!");
+			vendaFiado = false;
+        }else {
+			Console.WriteLine("Informe um valor válido!");
+        }
+    }
 }
