9d4026df0f7a0f505d5ca6a1a2aaab155053b0e3
Igor Henrique Rizzi <igor.rizzi2@gmail.com>
2021-09-16 14:37:24 -0300

    Update Funcionario.cs

--- C#/SysBodega/SysBodega/Funcionario.cs
+++ C#/SysBodega/SysBodega/Funcionario.cs
@@ -1,5 +1,15 @@
 ﻿using System;
 
 public class Funcionario{
-	
+	private String nomeFun;
+	private int codigoFun;
+	private int idade;
+	private Double salario;
+
+	public Funcionario(String nome, int numero, int idadeFun, Double salarioFun) {
+		nomeFun = nome;
+		codigoFun = numero;
+		idade = idadeFun;
+		salario = salarioFun;
+    }
 }
