d93c3c0d4274f2b1c66da3ebff3cd3cdeb144a21
Igor Henrique Rizzi <igor.rizzi2@gmail.com>
2021-09-16 14:37:22 -0300

    Create Empresa.cs

---
+++ C#/SysBodega/SysBodega/Empresa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Empresa{
+	private String nomeBodega = "Bodega do Juaumzinho!";
+	private String cnpjBodega = "02.789.657\0001-67";
+
+	public String getNomeBodega() {
+		return nomeBodega;
+    }
+	public String getCnpjBodega() {
+		return cnpjBodega;
+    }
+}
