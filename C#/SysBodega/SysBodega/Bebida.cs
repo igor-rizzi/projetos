b89c70f239ab473b965b7b2e59ca1a114687b5ce
Igor Henrique Rizzi <igor.rizzi2@gmail.com>
2021-09-16 14:37:19 -0300

    Update Bebida.cs

--- C#/SysBodega/SysBodega/Bebida.cs
+++ C#/SysBodega/SysBodega/Bebida.cs
@@ -1,8 +1,33 @@
 ﻿using System;
 
-public class Class1
-{
-	public Class1()
-	{
+class Bebida{
+	private int codigo;
+	private String nomeBebida;
+	private double teorAlcoolico;
+	private double qtdMl;
+	private double precoVenda;
+	private double qtdEstoque;
+
+	public Bebida(String nomeDrink, int codigoDrink, double teorAlcool, double ml, double preco) {
+		codigo = codigoDrink;
+		nomeBebida = nomeDrink;
+		teorAlcoolico = teorAlcool;
+		qtdMl = ml;
+		precoVenda = preco;
 	}
+
+	void vender(int quantidade) {
+		if(quantidade < qtdEstoque) {
+			qtdEstoque -= quantidade;
+			Console.WriteLine("A quantidade em estoque foi alterada com sucesso!");
+        } else {
+			Console.WriteLine("A quantidade de venda não pode ser maior do que a quantidade em estoque!");
+        }
+	}
+
+	void comprar(int quatidade) {
+		qtdEstoque += quantidade;
+		Console.WriteLine("A quantidade foi alterada com sucesso!");
+    }
+	
 }
